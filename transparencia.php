e0bf8e98ea9615e1c1d7c4d2efad3dcc397a0763
lindaiely <lindaielyrodrigues@gmail.com>
2023-05-10 14:58:45 -0300

    layout da tela de transparencia

--- transparencia.php
+++ transparencia.php
@@ -1,3 +1,43 @@
 <?php require_once "components/topo.php" ?>
 
+<div class="transparencia">
+    <h1>Prestação de contas / transparência</h1>
+        <table class="mytable">
+            <thead>
+                <tr>
+                    <th>DATA</th>
+                    <th>E/S</th>
+                    <th>R$</th>
+                    <th>DESCRIÇÃO</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr class="td-color">
+                    <td> - </td>
+                    <td> - </td>
+                    <td> - </td>
+                    <td> - </td>
+                </tr>
+                <tr class="td-color2">
+                    <td> - </td>
+                    <td> - </td>
+                    <td> - </td>
+                    <td> - </td>
+                </tr>
+                <tr class="td-color">
+                    <td> - </td>
+                    <td> - </td>
+                    <td> - </td>
+                    <td> - </td>
+                </tr>
+                <tr class="td-color2">
+                    <td> - </td>
+                    <td> - </td>
+                    <td> - </td>
+                    <td> - </td>
+                </tr>
+            </tbody>
+        </table>
+</div>
+
 <?php require_once "components/rodape.php" ?>
