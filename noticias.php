39d0a6d3a7f4e7eeb05ae392eef5780da0043a3d
lindaiely <lindaielyrodrigues@gmail.com>
2023-05-10 11:03:01 -0300

    tela de noticias

--- noticias.php
+++ noticias.php
@@ -1,13 +1,31 @@
 <?php require_once "components/topo.php" ?>
 
-<!-----
     <div class="noticia">
-    <h1 id="titulo-not">Título da notícia</h1>
+        <div id="titulo-not">
+            <h1>Título da notícia</h1>
+        </div>
         <div class="noticia-class">
             <img src="img/img-teste.png" alt="">
             <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolor quam consequatur illum fugit voluptatem sint eum error earum? Sapiente non adipisci consequuntur qui sunt voluptas quo beatae hic, ea error! Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste explicabo asperiores vel quo tempore obcaecati maiores ut sed repellat harum rerum voluptates, quibusdam provident laudantium, officiis, ullam necessitatibus recusandae odit?</p>
         </div>
     </div>
-    ---->
+    <div class="noticia">
+        <div id="titulo-not">
+            <h1>Título da notícia</h1>
+        </div>
+        <div class="noticia-class">
+            <img src="img/img-teste.png" alt="">
+            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolor quam consequatur illum fugit voluptatem sint eum error earum? Sapiente non adipisci consequuntur qui sunt voluptas quo beatae hic, ea error! Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste explicabo asperiores vel quo tempore obcaecati maiores ut sed repellat harum rerum voluptates, quibusdam provident laudantium, officiis, ullam necessitatibus recusandae odit?</p>
+        </div>
+    </div>
+    <div class="noticia">
+        <div id="titulo-not">
+            <h1>Título da notícia</h1>
+        </div>
+        <div class="noticia-class">
+            <img src="img/img-teste.png" alt="">
+            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolor quam consequatur illum fugit voluptatem sint eum error earum? Sapiente non adipisci consequuntur qui sunt voluptas quo beatae hic, ea error! Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste explicabo asperiores vel quo tempore obcaecati maiores ut sed repellat harum rerum voluptates, quibusdam provident laudantium, officiis, ullam necessitatibus recusandae odit?</p>
+        </div>
+    </div>
 
 <?php require_once "components/rodape.php" ?>
